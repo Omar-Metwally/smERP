b7eab5bd46615c06e371c99fd8430cbb237843ab
Omar <Omar@DESKTOP-ANILQRS>
2024-07-20 18:28:56 +0300

    AddingStorageLocation

---
+++ Domain/Entities/Organization/StorageLocation.cs
@@ -0,0 +1,9 @@
+﻿namespace smERP.Domain.Entities.Organization;
+
+public class StorageLocation
+{
+    public int Id { get; set; }
+    public int BranchId { get; set; }
+    public string Name { get; set; }
+    public virtual Branch Branch { get; set; }
+}
--- Domain/Entities/Transactions/ProductMoveTransaction.cs
+++ Domain/Entities/Transactions/ProductMoveTransaction.cs
@@ -1,17 +1,18 @@
 ﻿using smERP.Domain.Entities.Organization;
 using smERP.Domain.Entities.User;
+using System.Security.Cryptography.X509Certificates;
 
 namespace smERP.Domain.Entities.Transactions;
 
 public class ProductMoveTransaction : BaseTransaction
 {
-    public int FromBranchID { get; set; }
-    public int ToBranchID { get; set; }
+    public int FromStorageLocationID { get; set; }
+    public int ToStorageLocationID { get; set; }
     public int Quantity { get; set; }
     public string FromEmployeeID { get; set; }
     public string ToEmployeeID { get; set; }
-    public virtual Branch FromBranch { get; set; }
-    public virtual Branch ToBranch { get; set; }
+    public virtual StorageLocation FromStorageLocation { get; set; }
+    public virtual StorageLocation ToStorageLocation { get; set; }
     public virtual Employee FromEmployee { get; set; }
     public virtual Employee ToEmployee { get; set; }
 }
